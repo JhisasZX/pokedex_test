17176b8f3166d2e4c29adc0c013e513129df1927
Jhisas <jhxtone@gmail.com>
2025-04-16 23:01:47 -0400

    service-home_page/changes
    Increase the name limit to 150 (Pokédex service).
    Capitalize Pokémon names (Home page).

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,25 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:pokedex_test/services/pokedex_service.dart';
 
-/// This widget is the home page of the application.
-/// It is a stateless widget that displays a centered text "Home Page".
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+
+  String _capitalize(String text) {
+    if (text.isEmpty) return text;
+    return text[0].toUpperCase() + text.substring(1).toLowerCase();
+  }
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
       future: PokedexService().getPokemonsList(),
-      initialData: const [],
       builder: (BuildContext context, AsyncSnapshot snapshot) {
-        List<dynamic> pokemons = snapshot.data['results'];
-        return ListView.builder(
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else if (snapshot.hasData) {
+          final Map<String, dynamic> data = snapshot.data;
+          final List<dynamic> pokemons = data['results'];
+
+          return ListView.builder(
             itemCount: pokemons.length,
             itemBuilder: (BuildContext context, int index) {
               return ListTile(
-                title: Text(pokemons[index]['name']),
+                title: Text(_capitalize(pokemons[index]['name'])),
               );
-            });
+            },
+          );
+        } else {
+          return const Center(child: Text('No hay datos'));
+        }
       },
     );
   }
--- lib/services/pokedex_service.dart
+++ lib/services/pokedex_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class PokedexService {
-  String url = 'https://pokeapi.co/api/v2/pokemon?limit=50&offset=0';
+  String url = 'https://pokeapi.co/api/v2/pokemon?limit=150&offset=0';
   Future<dynamic> getPokemonsList() async {
     final response = await http.get(Uri.parse(url));
     final res = jsonDecode(response.body);
